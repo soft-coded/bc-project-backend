75afacd261c196dd9e91b32cdb4578f53dc78dcb
shrutanten <shrutanten.work@gmail.com>
2023-10-06 14:31:30 +0530

    Initial commit

---
+++ src/main/java/com/project/MultiCurrencyTransfer/MultiCurrencyTransferApplication.java
@@ -0,0 +1,13 @@
+package com.project.MultiCurrencyTransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiCurrencyTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiCurrencyTransferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/MultiCurrencyTransfer/MultiCurrencyTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.MultiCurrencyTransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiCurrencyTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
