4d7eed6398dde98788a29be09dc0b6eaa141c175
shrutanten <shrutanten.work@gmail.com>
2023-10-08 11:13:26 +0530

    Fix naming

--- src/main/java/com/project/MultiCurrencyTransfer/helpers/JwtUtil.java
+++ src/main/java/com/project/MultiCurrencyTransfer/helpers/JwtUtil.java
@@ -18,7 +18,7 @@ import java.util.Map;
 @Component
 public class JwtUtil {
 
-    private static final long serialVersionUID = -2550185165626007488L;
+    // private static final long serialVersionUID = -2550185165626007488L;
 
     public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
 
