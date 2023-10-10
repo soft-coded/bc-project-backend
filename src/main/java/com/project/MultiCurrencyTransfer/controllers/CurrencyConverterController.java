d9cd257be738b667aaf34980ae18368f2cbeb4d0
1192708 <anjali1912kumari@gmail.com>
2023-10-11 00:55:29 +0530

    done

--- src/main/java/com/project/MultiCurrencyTransfer/controllers/CurrencyConverterController.java
+++ src/main/java/com/project/MultiCurrencyTransfer/controllers/CurrencyConverterController.java
@@ -14,20 +14,47 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
-@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/user/")
 
 public class CurrencyConverterController {
-    // Add your API key and other necessary parameters
+    @CrossOrigin(origins = "*")
     @GetMapping("countrylist")
     public ResponseEntity<?> getCountryList()
     {
-        System.out.println("hello");
         String apiUrl = "https://restcountries.com/v3.1/all";
         RestTemplate restTemplate = new RestTemplate();
 
         try {
             ResponseEntity<String> response = restTemplate.getForEntity(apiUrl,String.class);
+            return response;
+        } catch (HttpClientErrorException e) {
+            // Handle client errors (e.g., 4xx status codes)
+            return ResponseEntity.status(e.getRawStatusCode()).body(e.getResponseBodyAsString());
+        } catch (ResourceAccessException e) {
+            // Handle network or resource acce
+            return ResponseEntity.status(500).body("Failed to connect to the external API.");
+        } catch (Exception e) {
+            // Handle other exceptions
+            return ResponseEntity.status(500).body("An error occurred while fetching data from the external API.");
+        }
+    }
+
+    @GetMapping("latest")
+    public ResponseEntity<?> getLatestCurrencyRates(@RequestParam("base_currency") String param1,
+                                                    @RequestParam("currencies") String param2)
+    {
+        String apiUrl = "https://api.freecurrencyapi.com/v1/latest";
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(apiUrl);
+
+        builder.queryParam("apikey", "fca_live_49jfgbN2zuQFEuPN1zO2POWSlUPvm7dgOj175GRN")
+                .queryParam("base_currency", param1)
+                .queryParam("currencies", param2);
+
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        try {
+            ResponseEntity<String> response = restTemplate.getForEntity(builder.toUriString(),String.class);
             return response;
         } catch (HttpClientErrorException e) {
             // Handle client errors (e.g., 4xx status codes)
--- src/test/java/com/project/MultiCurrencyTransfer/CurrencyConverterTests.java
+++ src/test/java/com/project/MultiCurrencyTransfer/CurrencyConverterTests.java
@@ -1,5 +1,4 @@
 package com.project.MultiCurrencyTransfer;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,16 +7,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
-
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//Initialization
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class CurrencyConverterTests {
@@ -36,22 +35,73 @@ public class CurrencyConverterTests {
 
     private String baseUrl;
 
-
     @BeforeEach
     public void setUp() {
-        // Configure mock responses for the RestTemplate
         baseUrl = "http://localhost:" + port;
         when(restTemplate.getForObject(anyString(), eq(String.class)))
                 .thenReturn("Mocked API Response JSON");
     }
 
-    //    Test if the restCountries API is working, and returning in required JSON format
     @Test
     public void testGetCountryList() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get(baseUrl + "/api/v1/user/countrylist")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    public void testGetLatestCurrencyRates_Success() throws Exception {
+        // Mock the external API response with a sample JSON
+        String sampleApiResponse = "{ \"data\": { \"AUD\": 1.5571402803 } }"; // Update the values
+        when(restTemplate.getForObject(anyString(), eq(String.class)))
+                .thenReturn(sampleApiResponse);
+
+        mockMvc.perform(MockMvcRequestBuilders.get(baseUrl + "/api/v1/user/latest")
+                        .param("base_currency", "USD")
+                        .param("currencies", "AUD")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json;charset=UTF-8"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.AUD").isNumber());
+    }
+
+
+
+    @Test
+    public void testConvertCurrency_NonExistentConversionRate() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/user/convert")
+                        .param("base_currency", "USD")
+                        .param("currencies", "JPY")
+                        .contentType("application/json;charset=UTF-8"))
+                .andExpect(MockMvcResultMatchers.status().isMethodNotAllowed());
+    }
+
+    @Test
+    public void testConvertCurrency_InvalidCurrencyCodes() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/user/convert")
+                        .param("base_currency", "XYZ")
+                        .param("currencies", "ABC")
+                        .contentType("application/json;charset=UTF-8"))
+                .andExpect(MockMvcResultMatchers.status().isMethodNotAllowed());
+    }
+
+    @Test
+    public void testConvertCurrency_UnsupportedMethod() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/user/convert")
+                        .param("base_currency", "USD")
+                        .param("currencies", "EUR")
+                        .contentType("application/json;charset=UTF-8"))
+                .andExpect(MockMvcResultMatchers.status().isMethodNotAllowed());
+    }
+
+    @Test
+    public void testConvertCurrency_MissingFromParameter() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/user/convert")
+                        .param("base_currency", "EUR")
+                        .param("currencies", "100.00")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isMethodNotAllowed());
     }
 
 }
